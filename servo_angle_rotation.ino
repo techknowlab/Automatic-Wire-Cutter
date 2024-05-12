d0ba023efc0c43fd2917d9749627cc9db7fde870
TechKnowLab <52469430+techknowlab@users.noreply.github.com>
2024-05-12 16:57:40 +0530

    Add files via upload
    Arduino code for automatic wire cutter

---
+++ servo_angle_rotation.ino
@@ -0,0 +1,47 @@
+#include <Servo.h>
+Servo servo1;
+int servoPin = 9;
+int Dir = 6;
+int Pul= 7;
+int arc = 15;
+int pd = 100;
+
+void setup(){
+  Serial.begin(9600);
+  servo1.attach(servoPin);
+  pinMode(Dir, OUTPUT);
+  pinMode(Pul, OUTPUT);
+}
+
+void loop(){
+  float s = 6.37*arc;
+  int steps = round(s);
+  servo1.write(0);
+  delay(1500);
+  
+  for(int i = 0; i <= steps; i++)
+  { 
+    Serial.println(i);
+    digitalWrite(Dir, HIGH);
+    delayMicroseconds(pd);
+    digitalWrite(Pul, HIGH);
+    delayMicroseconds(pd);
+    digitalWrite(Pul, LOW);
+    delayMicroseconds(pd);
+  }
+  delay(500);
+  servo1.write(120);
+  delay(1500);
+
+  // for(int i = 0; i <= 5; i++)
+  // { 
+  //   Serial.println(i);
+  //   digitalWrite(Dir, LOW);
+  //   delayMicroseconds(pd);
+  //   digitalWrite(Pul, HIGH);
+  //   delayMicroseconds(pd);
+  //   digitalWrite(Pul, LOW);
+  //   delayMicroseconds(pd);
+  // }
+  
+}
